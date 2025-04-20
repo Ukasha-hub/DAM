3a276a441b8aa95bb4fc67411c63349b3ed80688
Ukasha-hub <ukashajljl@gmail.com>
2025-04-20 16:36:26 +0600

    changes in folderItems

--- src/Pages/FolderItems.jsx
+++ src/Pages/FolderItems.jsx
@@ -57,7 +57,49 @@ const FolderItems = () => {
   }, [id]);
 
   if (!folder) return <div className="p-4">Folder not found.</div>;
-  if (items.length === 0) return <div className="p-4">This folder is empty.</div>;
+
+
+
+  if (items.length === 0) return <div className="p-4"> 
+  <div className='flex flex-col lg:flex-row lg:justify-between'>
+  <div className="breadcrumbs text-xs lg:text-sm">
+  <Breadcrumb location={location} pathnames={pathnames}></Breadcrumb>
+  </div>
+
+  <div>
+    <ul className='flex flex-row gap-7 lg:justify-around text-xs'>
+      <li><a href="">Folder Subscribe</a></li>
+      <li><a href="">Folder Settings</a></li>
+    </ul>
+  </div>
+</div>
+
+{/*buttons and paginations */}
+<div>
+<div className=" text-xs mt-1 lg:text-sm flex flex-col gap-1 lg:flex-row justify-evenly lg:justify-between">
+      <ul className='flex flex-row gap-2 lg:gap-3 '>
+          
+          <li><button onClick={() => navigate("/add-files")} className="btn-xs bg-green-300 p-1 rounded-sm">Add Files</button></li>
+          <li><button className='btn btn-xs' onClick={()=>{handleSelectAll(items)}}>Select all</button></li>
+          <li><a href="">Search within folder</a></li>
+          
+          
+          <li>
+              <div className="dropdown ">
+                      <div tabIndex={0} role="button" className="text-xs lg:text-sm lg:btn-sm ">More actions ▼</div>
+                      <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                          <li><a>Item 1</a></li>
+                          <li><a>Item 2</a></li>
+                      </ul>
+              </div>
+          </li>
+      </ul>
+      <PaginationComponent currentPage={currentPageinFiles} totalPages={totalPagesinFiles} onPageChange={setCurrentPageinFiles} handleSort={handleSortinFiles} sortBy={sortByinFiles} sortOrder={sortOrder} handleItemsPerPageChange={handleItemsPerPageChangeinFiles } itemsPerPage={itemsPerPageinFiles}></PaginationComponent>
+  </div>
+  <div className='flex justify-center pt-50'>Folder is empty</div>
+
+</div>
+</div>;
 
  
 
