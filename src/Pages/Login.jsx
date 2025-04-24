957cec948cd8feb7fa53ddd9462c2ec98aa29748
Ukasha-hub <ukashajljl@gmail.com>
2025-04-24 16:24:41 +0600

    login page design

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,26 +3,27 @@ import React from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { SiGithub } from "react-icons/si";
 import { FaSquareXTwitter } from "react-icons/fa6";
-
+import logo from '../assets/sysnova broadcast logo.png'; 
 const Login = () => {
   return (
-    <div>
-        <div className='flex justify-center items-center  p-full h-screen'>
-            <div className=" h-[80%] max-w-md p-8 space-y-3 rounded-xl bg-gray-100 text-gray-800">
+    <div className='flex flex-col justify-between  h-screen' style={{backgroundImage:"url('https://static.vecteezy.com/system/resources/previews/032/854/832/non_2x/modern-television-studio-for-world-breaking-news-with-equipment-for-leading-reporters-and-announcers-free-photo.jpg')",backgroundSize:'cover', backgroundRepeat:'no-repeat'}}>
+        
+        <div className='flex justify-center items-center h-full   '>
+            <div className=" h-[80%] max-w-md p-8 space-y-3 rounded-xl bg-gray-100 opacity-90 text-black shadow-black">
                 <h1 className="text-2xl font-bold text-center">Login</h1>
                 <form  action="" className="space-y-6">
                     <div className="space-y-1 text-sm">
                         <label htmlFor="username" className="block dark:text-gray-600">Username</label>
-                        <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800 focus:border-violet-600" />
+                        <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md border-2 border-black opacity-100 bg-white text-gray-800 " />
                     </div>
                     <div className="space-y-1 text-sm">
                         <label htmlFor="password" className="block text-gray-600">Password</label>
-                        <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800 focus:border-violet-600" />
+                        <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-2  border-black bg-white text-gray-800 " />
                         <div className="flex justify-end text-xs dark:text-gray-600">
                             <a rel="noopener noreferrer" href="#">Forgot Password?</a>
                         </div>
                     </div>
-                    <button  className="block w-full  p-3 text-center rounded-sm text-gray-50 bg-violet-600 active:bg-violet-700">Sign in</button>
+                    <button  className="block w-full  p-3 text-center rounded-sm text-gray-50 bg-blue-600 active:bg-blue-700">Sign in</button>
                 </form>
                 <div className="flex items-center pt-4 space-x-1">
                     <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
@@ -40,11 +41,19 @@ const Login = () => {
                 <FaSquareXTwitter  className='size-6'/>
                     </button>
                 </div>
-                <p className="text-xs text-center sm:px-6 dark:text-gray-600">Don't have an account?
-                    <a rel="noopener noreferrer" href="#" className="underline dark:text-gray-800">Sign up</a>
-                </p>
+               
             </div>
         </div>
+        <div>
+        <footer className="footer sm:footer-horizontal bg-gray-100 opacity-90 text-gray-800 justify-center p-4">
+            
+                
+                <div className='flex flex-row'><span>Powered by:</span> <img src={logo} alt="" className='h-7 w-30'/></div>
+            
+            
+        </footer>
+        </div>
+        
     </div>
   )
 }
